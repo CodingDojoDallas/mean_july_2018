dc790c3e625ccd193c127207fd8d08bae5def095
Micah Wu <micahwu@Micahs-MacBook-Pro.local>
2018-07-17 18:59:26 -0500

    angular restful_task push

---
+++ Micah_Wu/angular/restful_task/server.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var app = express();
+
+var path = require("path");
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public', 'dist', 'public')));
+//Database connection
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mongoose_dashboard');
+//Database Class Model
+var UserSchema = new mongoose.Schema({
+    animal: String,
+    name: String,
+    description: String,
+    date: Date
+  },{timestamps:true})
+  mongoose.model('User', UserSchema);
+  var User = mongoose.model('User');
+
+  app.get('/tasks', function(req, res) {
+    res.json({msg:'msg'})
+    })
+
+  app.get('/mongooses/new', function(req, res) {
+    res.render('create')
+    })
+  
+  app.post('/mongooses', function(req, res) {
+    console.log("POST DATA", req.body);
+  
+    var user = new User({animal: req.body.animal, name: req.body.name, description: req.body.description, date:req.body.date});
+    user.save(function(err) {
+  
+      if(err) {
+        console.log('something went wrong');
+      } else { // else console.log that we did well and then redirect to the root route
+        console.log('successfully added a user!');
+        res.redirect('/');
+      }
+    })
+  })
+  
+  app.get('/mongooses/:id', function(req, res) {
+    User.find({_id:req.params.id}, function(err, data) {
+      res.render('id',{users: data})
+    })
+  })
+  
+  app.get('/mongooses/edit/:id', function(req, res) {
+    User.find({_id:req.params.id}, function(err, data) {
+      console.log(data)
+      res.render('edit',{users:data})
+    })
+  })
+  
+  app.post('/mongooses/:id', function(req, res) {
+    console.log("POST DATA", req.body);
+    User.findOne({_id:req.params.id}, function(err, user){
+      if(err) { console.log('something went wrong'); }
+      else {
+        user.animal = req.body.animal;
+        user.name = req.body.name;
+        user.description = req.body.description;
+        user.date = req.body.date;
+        console.log(user)
+  
+        user.save(function(err){
+          if (err) console.log("failure")
+          else       console.log("success")
+        })
+        console.log(user)
+      }
+    })
+    res.redirect('/');
+  })
+  
+  // User.update({_id:req.params.id},{$push: {animal: req.body.animal, name: req.body.name, description: req.body.description, date:req.body.date}}, function(err){});
+  
+  app.get('/mongooses/destroy/:id', function(req, res) {
+    User.remove({_id:req.params.id}, function(err, data) {
+      res.redirect('/')
+    })
+  })
+
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
