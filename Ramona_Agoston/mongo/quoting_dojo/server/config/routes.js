0a5862a8caf91de024bc6eaf7f429fe612d9a8b1
RamonaFlorina <agoston.ramona@gmail.com>
2018-07-17 15:12:06 -0500

    Quoting_Dojo Controllers

--- Ramona_Agoston/mongo/quoting_dojo/server/config/routes.js
+++ Ramona_Agoston/mongo/quoting_dojo/server/config/routes.js
@@ -2,47 +2,12 @@
 var mongoose = require('mongoose')
 var Quote = mongoose.model("Quote")
 
+//require logic controllers
+const control = require('../controllers/quotes.js');
 
 //define and export routes to server.js
 module.exports = function (app) {
-    app.get('/', function(req, res) {
-        Quote.find({}, function(err, results) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            }
-            else {
-                console.log(results);
-                res.render('index', { data: results });
-            }
-        })
-    })
-
-    app.post('/add_quote', function(req, res) {
-        console.log(req.body);
-        var new_quote = new Quote(req.body);
-        new_quote.save(function(err, results) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            }
-            else {
-                console.log(results);
-                res.redirect('/');
-            }
-        })
-    })
-
-    app.get('/quotes', function(req, res) {
-        Quote.find({}).exec(function(err, quotes) {
-            if (!err) {
-                console.log("Show quotes");
-                res.render('quotes', { quotes: quotes })
-            }
-            else {
-                console.log("Error: dont show quotes");
-                res.render('quotes', { quotes: false });
-            }
-        })
-    })
+    app.get('/', control.index);
+    app.post('/add_quote', control.add_quote);
+    app.get('/quotes', control.quotes);
 }
