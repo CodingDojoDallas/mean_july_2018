28c2a22e557170e8ce72e1c4e86de060c79a7009
dgibs750 <doriangibson9663@gmail.com>
2018-07-18 21:03:43 -0500

    Updated api in 1955_api assignment

--- Dorian_Gibson/mongoDB/api/1955api/server.js
+++ Dorian_Gibson/mongoDB/api/1955api/server.js
@@ -7,8 +7,13 @@ let express     = require('express'),
 
 mongoose.connect('mongodb://localhost/1955_db');
 
-var NameSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
     name: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    job: {
         type: String,
         required: true,
         minlength: 2
@@ -18,12 +23,12 @@ var NameSchema = new mongoose.Schema({
 app.use(body_parser.json()); 
 app.use(express.static(path.join(__dirname, '/static')));
 
-mongoose.model('Name', NameSchema);
-var Name = mongoose.model('Name');
+mongoose.model('User', UserSchema);
+var User = mongoose.model('User');
 mongoose.Promise = global.Promise;
 
 app.get('/', function(req, res){
-    Name.find({}, function(err, quotes){
+    User.find({}, function(err, quotes){
         if(err){
            console.log("Returned error", err);
             // respond with JSON
@@ -36,17 +41,18 @@ app.get('/', function(req, res){
      })
 })
 
-app.get('/new/:name', function(req, res){
+app.get('/new/:name/:job', function(req, res){
     console.log(req.params.name);
-    var user = new Name();
+    var user = new User();
     user.name = req.params.name;
+    user.job = req.params.job;
     user.save();
     res.redirect('/');
 })
 
 app.get('/remove/:name', function(req, res){
     console.log(req.params.name);
-    Name.remove({name: req.params.name}, function(err){
+    User.remove({name: req.params.name}, function(err){
         res.redirect('/');
     })
 
@@ -54,7 +60,7 @@ app.get('/remove/:name', function(req, res){
 
 app.get('/:name', function(req, res){
     console.log(req.params.name);
-    Name.find({name: req.params.name}, function(err, quotes){
+    User.find({name: req.params.name}, function(err, quotes){
         if(err){
            console.log("Returned error", err);
             // respond with JSON
